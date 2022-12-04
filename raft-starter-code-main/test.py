729b6f4e381947d615b5a368384c9dec9599ae2d
justagoodfriend <greenate1@gmail.com>
2022-12-04 10:42:22 -0500

    new code with quorums and log stuff, a bit slow though

---
+++ raft-starter-code-main/test.py
@@ -0,0 +1,10 @@
+testarray = []
+entry = (1, 2, 3, 4)
+other = (entry, 5)
+testarray.append(other)
+print(testarray[0][0][3])
+
+
+testdict = {}
+testdict["something"] = 1
+#print(testdict["hello"])

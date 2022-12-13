d87b1a78c202778ee5d1b77e90070c387dc1b15e
justagoodfriend <greenate1@gmail.com>
2022-12-12 23:29:58 -0500

    partition 1/2 working fixing 3,4 tom

--- raft-starter-code-main/test.py
+++ raft-starter-code-main/test.py
@@ -2,7 +2,9 @@ testarray = []
 entry = (1, 2, 3, 4)
 other = (entry, 5)
 testarray.append(other)
-print(testarray[0][0][3])
+#print(testarray[0][0][3])
 
 testdict = {}
-testdict[("testing, test")] = ("testing12, testing123")
+testlist = [3, 2, 1, 0]
+for i in range(4, len(testlist)):
+    print(testlist[i])
